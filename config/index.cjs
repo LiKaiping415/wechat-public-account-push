6fa52ebe0827d7b8b657c7d88dcf220b823ea320
LiKaiping415 <111277655+LiKaiping415@users.noreply.github.com>
2024-03-30 22:12:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opV-t6PBTwOTBhe8plqCjPvWm8PA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oU8eMBxb7WF4Pilc_fCtpMFJKdWTuY-UQv-Err_TjyI',
+      useTemplateId: 'nYEeC960lPJeqfhMeeLdSudqOo4SscW0IqwdmX1rubo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-16',
       festivals: [
